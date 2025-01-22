c7c2eaf8e9eb8a428c886a22986cee301c3592c9
Alan <68080457+Alan-Barreto@users.noreply.github.com>
2025-01-22 17:20:52 +0100

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$db = mysqli_connect($_ENV['BD_HOST'], 
-$_ENV['BD_USER'], 
-$_ENV['BD_PASS'], 
-$_ENV['BD_NAME']
+$db = mysqli_connect($_ENV['DB_HOST'], 
+$_ENV['DB_USER'], 
+$_ENV['DB_PASS'], 
+$_ENV['DB_NAME']
 );
 
 
